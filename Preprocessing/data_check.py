0d9803aaecb169ba4a96bafcd4f77950786a27e6
MaxwellJChen <maxwelljchen1@gmail.com>
2023-01-18 13:16:04 -0500

    Began analyzing data.

--- Preprocessing/data_check.py
+++ Preprocessing/data_check.py
@@ -1,2 +1,5 @@
-print("HI!")
-print("Say goodbye!")
+import pandas as pd
+import numpy
+import pickle
+
+chembl = pd.read_csv("/Users/maxwellchen/PycharmProjects/RL_Drug_Generation/chembl_31_chemreps.txt", sep='\t', lineterminator='\r')
